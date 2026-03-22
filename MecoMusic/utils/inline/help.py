2b0a95a6da16c4d2f29527deae51e72ebc131aa3
rockyxd3 <shivamlkr629@gmail.com>
2026-03-22 15:41:14 +0530

    Update help.py

--- MecoMusic/utils/inline/help.py
+++ MecoMusic/utils/inline/help.py
@@ -28,70 +28,85 @@ def help_pannel(_, START: Union[bool, int] = None):
                 InlineKeyboardButton(
                     text=_["H_B_1"],
                     callback_data="help_callback hb1",
+                    style=ButtonStyle.SUCCESS,
                 ),
                 InlineKeyboardButton(
                     text=_["H_B_2"],
                     callback_data="help_callback hb2",
+                    style=ButtonStyle.SUCCESS,
                 ),
                 InlineKeyboardButton(
                     text=_["H_B_3"],
                     callback_data="help_callback hb3",
+                    style=ButtonStyle.SUCCESS,
                 ),
             ],
             [
                 InlineKeyboardButton(
                     text=_["H_B_4"],
                     callback_data="help_callback hb4",
+                    style=ButtonStyle.SUCCESS,
                 ),
                 InlineKeyboardButton(
                     text=_["H_B_5"],
                     callback_data="help_callback hb5",
+                    style=ButtonStyle.SUCCESS,
                 ),
                 InlineKeyboardButton(
                     text=_["H_B_6"],
                     callback_data="help_callback hb6",
+                    style=ButtonStyle.SUCCESS,
                 ),
             ],
             [
                 InlineKeyboardButton(
                     text=_["H_B_7"],
                     callback_data="help_callback hb7",
+                    style=ButtonStyle.SUCCESS,
                 ),
                 InlineKeyboardButton(
                     text=_["H_B_8"],
                     callback_data="help_callback hb8",
+                    style=ButtonStyle.SUCCESS,
                 ),
                 InlineKeyboardButton(
                     text=_["H_B_9"],
                     callback_data="help_callback hb9",
+                    style=ButtonStyle.SUCCESS,
                 ),
             ],
             [
                 InlineKeyboardButton(
                     text=_["H_B_10"],
                     callback_data="help_callback hb10",
+                    style=ButtonStyle.SUCCESS,
                 ),
                 InlineKeyboardButton(
                     text=_["H_B_11"],
                     callback_data="help_callback hb11",
+                    style=ButtonStyle.SUCCESS,
                 ),
                 InlineKeyboardButton(
                     text=_["H_B_12"],
                     callback_data="help_callback hb12",
+                    style=ButtonStyle.SUCCESS,
                 ),
             ],
             [
                 InlineKeyboardButton(
                     text=_["H_B_13"],
                     callback_data="help_callback hb13",
+                    style=ButtonStyle.SUCCESS,
                 ),
                 InlineKeyboardButton(
                     text=_["H_B_14"],
                     callback_data="help_callback hb14",
+                    style=ButtonStyle.SUCCESS,
                 ),
                 InlineKeyboardButton(
                     text=_["H_B_15"],
                     callback_data="help_callback hb15",
+                    style=ButtonStyle.SUCCESS,
                 ),
             ],
             mark,
@@ -121,7 +136,7 @@ def private_help_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_4"],
                 url=f"https://t.me/{app.username}?start=help",
-                style=ButtonStyle.PRIMARY,
+                style=ButtonStyle.SUCCESS,
             ),
         ],
     ]
