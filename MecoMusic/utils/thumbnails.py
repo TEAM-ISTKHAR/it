d23178bd8e152624f5eaa444e75961f7172ea650
rockyxd3 <shivamlkr629@gmail.com>
2026-04-05 13:51:33 +0530

    Update thumbnails.py

--- MecoMusic/utils/thumbnails.py
+++ MecoMusic/utils/thumbnails.py
@@ -177,7 +177,7 @@ async def get_thumb(videoid,user_id):
 
         youtube = Image.open(raw_thumb_path).convert("RGBA")
         background = fit_image(youtube, CANVAS_SIZE)
-        background = background.filter(ImageFilter.GaussianBlur(24))
+        background = background.filter(ImageFilter.GaussianBlur(16))
         background = ImageEnhance.Brightness(background).enhance(0.90)
         background = ImageEnhance.Color(background).enhance(0.70)
 
